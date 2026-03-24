f3837ea8e589fae7e5e2eedb60b6eb43e447768f
Abdulrahman Ahmed <ahmdhsnbdalrhmn357@gmail.com>
2026-03-25 00:00:13 +0200

    feat: Add phone number regex and validation function.

--- packages.py
+++ packages.py
@@ -6,6 +6,7 @@ import re
 class val:
     password_regex = r"(?P<password>((?=\S*[A-Z])(?=\S*[a-z])(?=\S*\d)(?=\S*[\!\"\§\$\%\&\/\(\)\=\?\+\*\#\'\^\°\,\;\.\:\<\>\ä\ö\ü\Ä\Ö\Ü\ß\?\|\@\~\´\`\\])\S{8,}))"
     email_regex = r"^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$"
+    phone_regex= r"/(?:([+]\d{1,4})[-.\s]?)?(?:[(](\d{1,3})[)][-.\s]?)?(\d{1,4})[-.\s]?(\d{1,4})[-.\s]?(\d{1,9})/g"
 
     def chk_p(password):
         # Checks for password containing at least 1 Upper case, 1 lower case, 1 digit and 1 special character,
@@ -18,6 +19,10 @@ class val:
         #Email address compliant with RFC2822
         __e = re.search(val.email_regex, email)
         if __e:
+            return True
+    def chk_ph(phone):
+        __ph = re.search(val.phone_regex, phone)
+        if __ph:
             return True
 
 def success_response(data):
